2993a3e99d41a38f2a2bd206372db4e8280345b0
Yogie Yadav <yogiejay21@gmail.com>
2024-01-30 16:12:00 +0530

    Implemented UI for Home page

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./Product.css";
-function Product(id, title, price, rating, image) {
-  return <div>Product</div>;
+function Product({ title, image, price, rating }) {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>{title}</p>
+        <p className="product__price">
+          <small>$</small>
+          <small>{price}</small>
+        </p>
+        <div className="product__rating">
+          <p>⭐️</p>
+          <p>⭐️</p>
+          <p>⭐️</p>
+        </div>
+        <img className="product__image" src={image} />
+      </div>
+      <button>Add to Basket</button>
+    </div>
+  );
 }
 
 export default Product;
